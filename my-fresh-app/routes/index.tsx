1e1466c691a57748dee4350921402616173fea22
Tejas Adagoor Manjunatha <tejas_adg@hotmail.com>
2025-05-09 08:34:49 -0400

    Refactor: Update layout and styling of menu items in HeroGridPage

--- my-fresh-app/routes/index.tsx
+++ my-fresh-app/routes/index.tsx
@@ -48,19 +48,18 @@ export default function HeroGridPage() {
         </div>
 
         <div
-          className="min-h-screen bg-cover bg-center bg-[url('/comic.png')] text-white flex items-center justify-center p-12 relative font-redrose"
+          className="h-screen w-full bg-cover bg-center bg-[url('/comic.png')] text-white flex items-center justify-center p-12 relative font-redrose"
         >
           <Rectangle height="75%" width="100%" className=" absolute inset-x-0 top-0 z-0 bg-indigo-black-fade"/>
           <Rectangle height="25%" width="100%" className=" absolute inset-x-0 bottom-0 z-0 bg-black-indigo-fade"/>
-            <div className="relative z-10 grid grid-cols-4 grid-rows-3 gap-12 text-center w-1/2 mx-auto">
-              {menuItems.map(({ title, desc }) => (
-              <a href={`/${title.toLowerCase()}`} className="">
-                <h2 className="text-6xl font-bold">{title}</h2>
-                <p className="text-xl">{desc}</p>
-              </a>
-              ))}
-
-            </div>
+          <div className="relative z-10 grid grid-cols-4 grid-rows-3 gap-6 gap-y-12 xl:gap-12 text-center 2xl:w-4/5 mx-auto">
+            {menuItems.map(({ title, desc }) => (
+            <a href={`/${title.toLowerCase()}`} className="">
+              <h2 className=" text-4xl xl:text-5xl 2xl:text-6xl font-bold">{title}</h2>
+              <p className=" text-sm xl:text-base 2xl:text-xl">{desc}</p>
+            </a>
+            ))}
+          </div>
 
           <div className="absolute bottom-4 left-4 text-lg">
             © Disruptor LLC, all rights reserved
